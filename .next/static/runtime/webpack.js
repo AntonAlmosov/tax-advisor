6106d4ecd2e0f35feef8a9810cf8b4d87d2985f0
Almosov Anton <almosoww@gmail.com>
2019-03-29 18:10:29 +0300

    Started to work on taxes page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a537270b43075cceb644";
+/******/ 	var hotCurrentHash = "7b6ef1c916585f7b6ca0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
