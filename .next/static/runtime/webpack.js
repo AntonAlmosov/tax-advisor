81925db463468b3fe1f3243fa9e8294f642078ef
Almosov Anton <almosoww@gmail.com>
2019-04-01 17:45:27 +0300

    Finished filling taxes.json

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "297dd858368be7a64fd8";
+/******/ 	var hotCurrentHash = "8076684044187445c32e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/taxes.js
+++ pages/taxes.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import Head from 'next/head'
 
+import data from './taxes.json'
 import Header from './../components/Header'
 import './../style.scss'
 
 class Taxes extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            data
+        }
+    }
     render(){
         return(
             <div>
+                <Head>
+                    <title>Налоговый советник | 3-НДФЛ</title>
+                </Head>
                 <Header />
+                {data.services}
             </div>
         )
     }
