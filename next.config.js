ec4d2b1ea7eb7989b637f45c36a17d6274241aa0
Almosov Anton <almosoww@gmail.com>
2019-04-05 13:32:51 +0300

    Deploying to the now

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
     cssModules: true,
-    // target: 'serverless',
-    exportPathMap: function() {
-        return {
-          '/': { page: '/' },
-          '/enterprenuer': { page: '/enterprenuer' },
-          '/organizations': { page: '/organizations' },
-          '/taxes': { page: '/taxes' }
-        }
-      },
+    target: 'serverless',
+    // exportPathMap: function() {
+    //     return {
+    //       '/': { page: '/' },
+    //       '/enterprenuer': { page: '/enterprenuer' },
+    //       '/organizations': { page: '/organizations' },
+    //       '/taxes': { page: '/taxes' }
+    //     }
+    //   },
     webpack (config, options) {
     config.module.rules.push({
       test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
